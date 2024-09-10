027bbbe86e6d7c34ad5003035a1fca260aa7edf7
Divine <officialkb17@gmail.com>
2024-09-10 14:30:47 +0100

    portfolio summary ui barebones

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,15 +1,48 @@
+"use client";
 
-import React from 'react';
-import '../../../styles/pages/Dashboard.scss';
-import Header from '../../../components/Header';
-import TabNavigation from '../../../components/TabNavigation';
-
+import React, { useState } from "react";
+import "../../../styles/pages/Portfolio.scss";
+import Header from "../../../components/Header";
+import TabNavigation from "../../../components/TabNavigation";
 
 const Portfolio = () => {
+  const [activeTab, setActiveTab] = useState("personal");
+
   return (
-    <div className="dashboard">
+    <div className="portfolio">
       <Header type="dashboard" />
-      <p>Portfolio</p>
+      <div className="portfolio__top">
+        <p className="portfolio__top__text">Portfolio</p>
+      </div>
+      <div className="portfolio__cont">
+        <p className="portfolio__cont__heading">Portfolio Summary</p>
+
+        <div
+          className={`portfolio__cont__tab ${
+            activeTab === "personal"
+              ? "personal-active"
+              : "protocol-wide-active"
+          }`}
+        >
+          <div className="portfolio__cont__tab__slider"></div>
+          <div
+            className={`portfolio__cont__tab__personal ${
+              activeTab === "personal" ? "active" : ""
+            }`}
+            onClick={() => setActiveTab("personal")}
+          >
+            Personal
+          </div>
+          <div
+            className={`portfolio__cont__tab__protocol-wide ${
+              activeTab === "protocol-wide" ? "active" : ""
+            }`}
+            onClick={() => setActiveTab("protocol-wide")}
+          >
+            Protocol wide
+          </div>
+        </div>
+      </div>
       <TabNavigation />
     </div>
   );
