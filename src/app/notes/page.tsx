2117faab374dd56153ae9ad0801edc2d56c856d0
Divine <officialkb17@gmail.com>
2024-09-16 21:49:46 +0100

    Build ui of notes left section

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../../../styles/pages/Notes.scss";
 import Header from "../../../components/Header";
 import TabNavigation from "../../../components/TabNavigation";
+import pencil from "../../../assets/svgs/edit-2.svg";
+import Image from "next/image";
 
 const Notes = () => {
   return (
@@ -14,7 +16,44 @@ const Notes = () => {
         </p>
       </div>
       <div className="notes__wrapper">
-        <div className="notes__wrapper__left" />
+        <div className="notes__wrapper__left">
+          <button className="notes__wrapper__left__button">
+            Make a new note
+          </button>
+          <div className="notes__wrapper__left__stack">
+            <div className="notes__wrapper__left__stack__note">
+              <div className="notes__wrapper__left__stack__note__top">
+                <p className="notes__wrapper__left__stack__note__top__text">
+                  BTC/USDC Ideas
+                </p>
+                <Image
+                  src={pencil}
+                  alt="edit icon"
+                  className="notes__wrapper__left__stack__note__top__icon"
+                />
+              </div>
+              <p className="notes__wrapper__left__stack__note__details">
+                Make a new note
+              </p>
+            </div>
+
+            <div className="notes__wrapper__left__stack__note">
+              <div className="notes__wrapper__left__stack__note__top">
+                <p className="notes__wrapper__left__stack__note__top__text">
+                  wstETH/ETH
+                </p>
+                <Image
+                  src={pencil}
+                  alt="edit icon"
+                  className="notes__wrapper__left__stack__note__top__icon"
+                />
+              </div>
+              <p className="notes__wrapper__left__stack__note__details">
+                What this basically entails is...
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="notes__wrapper__right" />
       </div>
       <TabNavigation />
